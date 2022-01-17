d386a5eeb3107228b493b221db9d69c50e5167b9
walter sun <swh7941@qq.com>
2022-01-17 19:29:50 +0800

    【*】go web

--- go_project/main/main.go
+++ go_project/main/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"strings"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
 	fmt.Printf("hello world")
 	fmt.Printf("strings.contains ", strings.Contains("asc", "s"))
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
