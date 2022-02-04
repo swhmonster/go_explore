16b6755d521ef05c7b0ebf070fd897d5fb94db04
道禹 <sunwenhao.swh@alibaba-inc.com>
2022-02-04 19:41:58 +0800

    【*】file http explore

--- go_project/main/main.go
+++ go_project/main/main.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func main() {
-	fmt.Printf("hello world")
-	fmt.Printf("strings.contains ", strings.Contains("asc", "s"))
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := "/Users/sunwenhao/Downloads/temp/1.txt"
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(data)
+	fmt.Fprintf(w, filePath)
+}
 
+func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/file", fileHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
