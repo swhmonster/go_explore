91b5ef105cab811560ce142ec35978990f2d9729
道禹 <sunwenhao.swh@alibaba-inc.com>
2022-03-07 13:06:24 +0800

    【*】go json序列化、反序列化

--- go_project/main/main.go
+++ go_project/main/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
+
+type Student struct {
+	Name   string   `json:"name"`   // 姓名
+	Age    int      `json:"age"`    // 年龄
+	Gender string   `json:"gender"` // 性别
+	Score  float64  `json:"score"`  // 分数
+	Course []string `json:"course"` // 课程
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
@@ -21,8 +30,38 @@ func fileHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, filePath)
 }
 
+func serialize() {
+	stu := Student{
+		"张三",
+		20,
+		"男",
+		78.6,
+		[]string{"语文", "数学", "音乐"},
+	}
+
+	// 序列化
+	data, err := json.Marshal(&stu)
+	if err != nil {
+		fmt.Println("序列化错误", err)
+	} else {
+		fmt.Println("序列化结果：" + string(data))
+	}
+
+	// 反序列化
+	var stu2 Student
+	err2 := json.Unmarshal(data, &stu2)
+	if err2 != nil {
+		fmt.Println("反序列化失败", err2)
+	} else {
+		fmt.Printf("反序列化结果：")
+		fmt.Println(stu2)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/file", fileHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	serialize()
+
+	/*http.HandleFunc("/", handler)
+	//http.HandleFunc("/file", fileHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))*/
 }
