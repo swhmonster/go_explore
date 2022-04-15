f6fb698f02f69973ed5796e8ef5598476a634efc
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2022-04-15 15:08:17 +0800

    【*】generic：type assert

--- go_project/src/demotest/test.go
+++ go_project/src/demotest/test.go
@@ -100,3 +100,34 @@ func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
 	}
 	return s
 }
+
+// type assert demo
+// Container is a generic container, accepting anything.
+type Container []interface{}
+
+//Put adds an element to the container.
+func (c *Container) Put(elem interface{}) {
+	*c = append(*c, elem)
+}
+
+//Get gets an element from the container.
+func (c *Container) Get() interface{} {
+	elem := (*c)[0]
+	*c = (*c)[1:]
+	return elem
+}
+
+/*
+// common use
+intContainer := &Container{}
+intContainer.Put(7)
+intContainer.Put(42)
+
+// type assert use
+// assert that the actual type is int
+elem, ok := intContainer.Get().(int)
+if !ok {
+    fmt.Println("Unable to read an int from intContainer")
+}
+fmt.Printf("assertExample: %d (%T)\n", elem, elem)
+*/
